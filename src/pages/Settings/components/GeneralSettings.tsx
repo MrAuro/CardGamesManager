MrAuro/CardGamesManager

commit 23d43d4ed5aea2aadce26fca9ec094b3d0ca8909

ui: Allow decimals for Blackjack payouts

src/pages/Settings/components/GeneralSettings.tsx
@@ -1,4 +1,4 @@
-import { KEYBINDINGS_STATE, PLAYERS_STATE, SETTINGS_STATE } from "@/Root";
+import { KEYBINDINGS_STATE, PLAYERS_STATE, SETTINGS_STATE, TAURI_STORE } from "@/Root";
 import { Scope, Scopes, getActions } from "@/types/Keybindings";
 import { Player } from "@/types/Player";
 import { DefaultKeybinds } from "@/utils/DefaultKeybinds";
@@ -7,7 +7,6 @@ import {
   ActionIcon,
   Button,
   Code,
-  Collapse,
   Grid,
   Input,
   Select,
@@ -17,14 +16,7 @@ import {
   Title,
 } from "@mantine/core";
 import { useDisclosure } from "@mantine/hooks";
-import {
-  IconDeviceFloppy,
-  IconKeyframe,
-  IconKeyframeFilled,
-  IconPencil,
-  IconPlus,
-  IconTrash,
-} from "@tabler/icons-react";
+import { IconKeyframe, IconKeyframeFilled, IconPlus, IconTrash } from "@tabler/icons-react";
 import { useState } from "react";
 import { useRecordHotkeys } from "react-hotkeys-hook";
 import { useRecoilState } from "recoil";
@@ -36,7 +28,6 @@ export default function GeneralSettings() {
 
   const [opened, { toggle }] = useDisclosure(false);
   const [keylistening, setKeylistening] = useState("");
-  const [keyEditing, setKeyEditing] = useState("");
   const [keys, { start, stop }] = useRecordHotkeys();
 
   const saveKey = (id: string, key: string) => {
@@ -97,6 +88,17 @@ export default function GeneralSettings() {
         populate players
       </Button>
       <Button
+        mt="sm"
+        variant="light"
+        color="red"
+        onClick={() => {
+          TAURI_STORE.clear();
+          window.location.reload();
+        }}
+      >
+        Reset data
+      </Button>
+      <Button
         onClick={() => {
           setKeybindings([...DefaultKeybinds]);
         }}
@@ -145,7 +147,7 @@ export default function GeneralSettings() {
       </Input.Wrapper>
       {/* <Text fw="bold">Keyboard Shortcuts</Text> */}
       <Button onClick={toggle}>{opened ? "Hide" : "Show"} Keyboard Shortcuts</Button>
-      <Collapse in={opened}>
+      {opened && (
         <Table
           withColumnBorders
           withTableBorder
@@ -161,7 +163,7 @@ export default function GeneralSettings() {
               <Table.Th>Action</Table.Th>
               <Table.Th
                 style={{
-                  width: "80px",
+                  width: "50px",
                 }}
               />
             </Table.Tr>
@@ -205,105 +207,72 @@ export default function GeneralSettings() {
                           alignItems: "center",
                         }}
                       >
-                        {keyEditing === keybinding.id && (
-                          <ActionIcon
-                            variant="transparent"
-                            c="gray"
-                            onClick={() => {
-                              if (keylistening === keybinding.id) {
-                                saveKey(keybinding.id, Array.from(keys).join("+"));
+                        <ActionIcon
+                          variant="transparent"
+                          c="gray"
+                          onClick={() => {
+                            if (keylistening === keybinding.id) {
+                              saveKey(keybinding.id, Array.from(keys).join("+"));
 
-                                setKeylistening("");
-                                stop();
-                              } else {
-                                saveKey(keybinding.id, "");
+                              setKeylistening("");
+                              stop();
+                            } else {
+                              saveKey(keybinding.id, "");
 
-                                setKeylistening(keybinding.id);
-                                start();
-                              }
-                            }}
-                          >
-                            {keylistening === keybinding.id ? (
-                              <IconKeyframeFilled />
-                            ) : (
-                              <IconKeyframe />
-                            )}
-                          </ActionIcon>
-                        )}
+                              setKeylistening(keybinding.id);
+                              start();
+                            }
+                          }}
+                        >
+                          {keylistening === keybinding.id ? (
+                            <IconKeyframeFilled />
+                          ) : (
+                            <IconKeyframe />
+                          )}
+                        </ActionIcon>
                       </Grid.Col>
                     </Grid>
                   </Table.Td>
 
-                  <Table.Td
-                    style={{
-                      height: 51, // Prevents the height from changing when the select is shown
-                    }}
-                  >
-                    {keyEditing === keybinding.id ? (
-                      <Select
-                        searchable
-                        data={Scopes}
-                        defaultValue={keybinding.scope}
-                        allowDeselect={false}
-                        onChange={(value) => saveScope(keybinding.id, value as Scope)}
-                      />
-                    ) : (
-                      <Text size="sm" c="dimmed">
-                        {keybinding.scope}
-                      </Text>
-                    )}
+                  <Table.Td>
+                    <Select
+                      data={Scopes}
+                      defaultValue={keybinding.scope}
+                      allowDeselect={false}
+                      onChange={(value) => {
+                        saveScope(keybinding.id, value as Scope);
+                      }}
+                    />
                   </Table.Td>
                   <Table.Td>
-                    {keyEditing === keybinding.id ? (
-                      <Select
-                        searchable
-                        data={getActions(keybinding.scope)}
-                        defaultValue={keybinding.action}
-                        allowDeselect={false}
-                        onChange={(value) => saveAction(keybinding.id, value as string)}
-                      />
-                    ) : (
-                      <Text size="sm" c="dimmed">
-                        {keybinding.action}
-                      </Text>
-                    )}
+                    <Select
+                      searchable
+                      data={getActions(keybinding.scope)}
+                      defaultValue={keybinding.action}
+                      allowDeselect={false}
+                      onChange={(value) => saveAction(keybinding.id, value as string)}
+                    />
                   </Table.Td>
                   <Table.Td
                     py={0}
                     style={{
-                      width: "80px",
+                      width: "40px",
                     }}
                   >
                     <div
                       style={{
                         display: "flex",
-                        justifyContent: "center",
+                        justifyContent: "flex-end",
                         alignItems: "center",
-                        gap: 5,
                       }}
                     >
                       <ActionIcon
                         variant="transparent"
-                        c="gray"
-                        onClick={() => {
-                          if (keyEditing === keybinding.id) {
-                            setKeyEditing("");
-                          } else {
-                            setKeyEditing(keybinding.id);
-                          }
-                        }}
+                        c="red"
+                        onClick={() => deleteKeybinding(keybinding.id)}
                       >
-                        {keyEditing === keybinding.id ? <IconDeviceFloppy /> : <IconPencil />}
+                        <IconTrash />
                       </ActionIcon>
-                      {keyEditing === keybinding.id && (
-                        <ActionIcon
-                          variant="transparent"
-                          c="red"
-                          onClick={() => deleteKeybinding(keybinding.id)}
-                        >
-                          <IconTrash />
-                        </ActionIcon>
-                      )}
                     </div>
                   </Table.Td>
                 </Table.Tr>
@@ -330,7 +299,7 @@ export default function GeneralSettings() {
             </Button>
           </Table.Caption>
         </Table>
-      </Collapse>
+      )}
     </>
   );
 }

src/Root.tsx
@@ -76,7 +76,7 @@ export const BLACKJACK_SETTINGS = atom<BlackjackSettings>({
         splitAces: true,
         splitAcesReceiveOneCard: true,
 
-        blackjackPayout: 3 / 2,
+        blackjackPayout: 1.5,
 
         twentyOnePlusThreeEnabled: false,
         twentyOnePlusThreeFlushPayout: 5,
@@ -94,6 +94,7 @@ export const BLACKJACK_SETTINGS = atom<BlackjackSettings>({
       };
       await TAURI_STORE.set("blackjackSettings", settings);
     }
+    console.log(settings);
 
     resolve(settings as BlackjackSettings);
   }),

src/pages/Settings/components/BlackjackSettings.tsx
@@ -7,6 +7,8 @@ export default function BlackjackSettings() {
   const [blackjackSettings, setBlackjackSettings] = useRecoilState(BLACKJACK_SETTINGS);
   const [blackjackGame] = useRecoilState(BLACKJACK_GAME_STATE);
 
+  console.log(blackjackSettings.blackjackPayout, "aaa");
+
   return (
     <>
       <Title order={2}>Blackjack Settings</Title>
@@ -21,6 +23,8 @@ export default function BlackjackSettings() {
             placeholder="0"
             disabled={blackjackGame.gameState != "PREROUND"}
             value={blackjackSettings.blackjackPayout}
+            decimalScale={2}
+            fixedDecimalScale
             onChange={(value) =>
               setBlackjackSettings({
                 ...blackjackSettings,
@@ -68,6 +72,8 @@ export default function BlackjackSettings() {
                 placeholder="0"
                 disabled={blackjackGame.gameState != "PREROUND"}
                 value={blackjackSettings.perfectPairsMixedPayout}
+                decimalScale={2}
+                fixedDecimalScale
                 onChange={(value) => {
                   setBlackjackSettings({
                     ...blackjackSettings,
@@ -86,6 +92,8 @@ export default function BlackjackSettings() {
                 placeholder="0"
                 disabled={blackjackGame.gameState != "PREROUND"}
                 value={blackjackSettings.perfectPairsColoredPayout}
+                decimalScale={2}
+                fixedDecimalScale
                 onChange={(value) => {
                   setBlackjackSettings({
                     ...blackjackSettings,
@@ -106,6 +114,8 @@ export default function BlackjackSettings() {
                 placeholder="0"
                 disabled={blackjackGame.gameState != "PREROUND"}
                 value={blackjackSettings.perfectPairsSuitedPayout}
+                decimalScale={2}
+                fixedDecimalScale
                 onChange={(value) =>
                   setBlackjackSettings({
                     ...blackjackSettings,
@@ -152,6 +162,8 @@ export default function BlackjackSettings() {
                 placeholder="0"
                 disabled={blackjackGame.gameState != "PREROUND"}
                 value={blackjackSettings.twentyOnePlusThreeFlushPayout}
+                decimalScale={2}
+                fixedDecimalScale
                 onChange={(value) =>
                   setBlackjackSettings({
                     ...blackjackSettings,
@@ -170,6 +182,8 @@ export default function BlackjackSettings() {
                 placeholder="0"
                 disabled={blackjackGame.gameState != "PREROUND"}
                 value={blackjackSettings.twentyOnePlusThreeStraightPayout}
+                decimalScale={2}
+                fixedDecimalScale
                 onChange={(value) => {
                   setBlackjackSettings({
                     ...blackjackSettings,
@@ -190,6 +204,8 @@ export default function BlackjackSettings() {
                 placeholder="0"
                 disabled={blackjackGame.gameState != "PREROUND"}
                 value={blackjackSettings.twentyOnePlusThreeThreeOfAKindPayout}
+                decimalScale={2}
+                fixedDecimalScale
                 onChange={(value) => {
                   setBlackjackSettings({
                     ...blackjackSettings,
@@ -208,6 +224,8 @@ export default function BlackjackSettings() {
                 placeholder="0"
                 disabled={blackjackGame.gameState != "PREROUND"}
                 value={blackjackSettings.twentyOnePlusThreeStraightFlushPayout}
+                decimalScale={2}
+                fixedDecimalScale
                 onChange={(value) => {
                   setBlackjackSettings({
                     ...blackjackSettings,
@@ -228,6 +246,8 @@ export default function BlackjackSettings() {
                 placeholder="0"
                 disabled={blackjackGame.gameState != "PREROUND"}
                 value={blackjackSettings.twentyOnePlusThreeThreeOfAKindSuitedPayout}
+                decimalScale={2}
+                fixedDecimalScale
                 onChange={(value) => {
                   setBlackjackSettings({
                     ...blackjackSettings,

commit faf5a42b0dffcda02b5d2b83472fecd106efb1fc

dev: Revert accidental rollback GeneralSettings.tsx
Not sure what happened here, but 23d43d4ed5aea2aadce26fca9ec094b3d0ca8909 accidentally undid all the changes I had to this file

src/pages/Settings/components/GeneralSettings.tsx
@@ -1,4 +1,4 @@
-import { KEYBINDINGS_STATE, PLAYERS_STATE, SETTINGS_STATE, TAURI_STORE } from "@/Root";
+import { KEYBINDINGS_STATE, PLAYERS_STATE, SETTINGS_STATE } from "@/Root";
 import { Scope, Scopes, getActions } from "@/types/Keybindings";
 import { Player } from "@/types/Player";
 import { DefaultKeybinds } from "@/utils/DefaultKeybinds";
@@ -7,6 +7,7 @@ import {
   ActionIcon,
   Button,
   Code,
+  Collapse,
   Grid,
   Input,
   Select,
@@ -16,7 +17,14 @@ import {
   Title,
 } from "@mantine/core";
 import { useDisclosure } from "@mantine/hooks";
-import { IconKeyframe, IconKeyframeFilled, IconPlus, IconTrash } from "@tabler/icons-react";
+import {
+  IconDeviceFloppy,
+  IconKeyframe,
+  IconKeyframeFilled,
+  IconPencil,
+  IconPlus,
+  IconTrash,
+} from "@tabler/icons-react";
 import { useState } from "react";
 import { useRecordHotkeys } from "react-hotkeys-hook";
 import { useRecoilState } from "recoil";
@@ -28,6 +36,7 @@ export default function GeneralSettings() {
 
   const [opened, { toggle }] = useDisclosure(false);
   const [keylistening, setKeylistening] = useState("");
+  const [keyEditing, setKeyEditing] = useState("");
   const [keys, { start, stop }] = useRecordHotkeys();
 
   const saveKey = (id: string, key: string) => {
@@ -88,17 +97,6 @@ export default function GeneralSettings() {
         populate players
       </Button>
       <Button
-        mt="sm"
-        variant="light"
-        color="red"
-        onClick={() => {
-          TAURI_STORE.clear();
-          window.location.reload();
-        }}
-      >
-        Reset data
-      </Button>
-      <Button
         onClick={() => {
           setKeybindings([...DefaultKeybinds]);
         }}
@@ -147,7 +145,7 @@ export default function GeneralSettings() {
       </Input.Wrapper>
       {/* <Text fw="bold">Keyboard Shortcuts</Text> */}
       <Button onClick={toggle}>{opened ? "Hide" : "Show"} Keyboard Shortcuts</Button>
-      {opened && (
+      <Collapse in={opened}>
         <Table
           withColumnBorders
           withTableBorder
@@ -163,7 +161,7 @@ export default function GeneralSettings() {
               <Table.Th>Action</Table.Th>
               <Table.Th
                 style={{
-                  width: "50px",
+                  width: "80px",
                 }}
               />
             </Table.Tr>
@@ -207,72 +205,105 @@ export default function GeneralSettings() {
                           alignItems: "center",
                         }}
                       >
-                        <ActionIcon
-                          variant="transparent"
-                          c="gray"
-                          onClick={() => {
-                            if (keylistening === keybinding.id) {
-                              saveKey(keybinding.id, Array.from(keys).join("+"));
+                        {keyEditing === keybinding.id && (
+                          <ActionIcon
+                            variant="transparent"
+                            c="gray"
+                            onClick={() => {
+                              if (keylistening === keybinding.id) {
+                                saveKey(keybinding.id, Array.from(keys).join("+"));
 
-                              setKeylistening("");
-                              stop();
-                            } else {
-                              saveKey(keybinding.id, "");
+                                setKeylistening("");
+                                stop();
+                              } else {
+                                saveKey(keybinding.id, "");
 
-                              setKeylistening(keybinding.id);
-                              start();
-                            }
-                          }}
-                        >
-                          {keylistening === keybinding.id ? (
-                            <IconKeyframeFilled />
-                          ) : (
-                            <IconKeyframe />
-                          )}
-                        </ActionIcon>
+                                setKeylistening(keybinding.id);
+                                start();
+                              }
+                            }}
+                          >
+                            {keylistening === keybinding.id ? (
+                              <IconKeyframeFilled />
+                            ) : (
+                              <IconKeyframe />
+                            )}
+                          </ActionIcon>
+                        )}
                       </Grid.Col>
                     </Grid>
                   </Table.Td>
 
-                  <Table.Td>
-                    <Select
-                      data={Scopes}
-                      defaultValue={keybinding.scope}
-                      allowDeselect={false}
-                      onChange={(value) => {
-                        saveScope(keybinding.id, value as Scope);
-                      }}
-                    />
+                  <Table.Td
+                    style={{
+                      height: 51, // Prevents the height from changing when the select is shown
+                    }}
+                  >
+                    {keyEditing === keybinding.id ? (
+                      <Select
+                        searchable
+                        data={Scopes}
+                        defaultValue={keybinding.scope}
+                        allowDeselect={false}
+                        onChange={(value) => saveScope(keybinding.id, value as Scope)}
+                      />
+                    ) : (
+                      <Text size="sm" c="dimmed">
+                        {keybinding.scope}
+                      </Text>
+                    )}
                   </Table.Td>
                   <Table.Td>
-                    <Select
-                      searchable
-                      data={getActions(keybinding.scope)}
-                      defaultValue={keybinding.action}
-                      allowDeselect={false}
-                      onChange={(value) => saveAction(keybinding.id, value as string)}
-                    />
+                    {keyEditing === keybinding.id ? (
+                      <Select
+                        searchable
+                        data={getActions(keybinding.scope)}
+                        defaultValue={keybinding.action}
+                        allowDeselect={false}
+                        onChange={(value) => saveAction(keybinding.id, value as string)}
+                      />
+                    ) : (
+                      <Text size="sm" c="dimmed">
+                        {keybinding.action}
+                      </Text>
+                    )}
                   </Table.Td>
                   <Table.Td
                     py={0}
                     style={{
-                      width: "40px",
+                      width: "80px",
                     }}
                   >
                     <div
                       style={{
                         display: "flex",
-                        justifyContent: "flex-end",
+                        justifyContent: "center",
                         alignItems: "center",
+                        gap: 5,
                       }}
                     >
                       <ActionIcon
                         variant="transparent"
-                        c="red"
-                        onClick={() => deleteKeybinding(keybinding.id)}
+                        c="gray"
+                        onClick={() => {
+                          if (keyEditing === keybinding.id) {
+                            setKeyEditing("");
+                          } else {
+                            setKeyEditing(keybinding.id);
+                          }
+                        }}
                       >
-                        <IconTrash />
+                        {keyEditing === keybinding.id ? <IconDeviceFloppy /> : <IconPencil />}
                       </ActionIcon>
+                      {keyEditing === keybinding.id && (
+                        <ActionIcon
+                          variant="transparent"
+                          c="red"
+                          onClick={() => deleteKeybinding(keybinding.id)}
+                        >
+                          <IconTrash />
+                        </ActionIcon>
+                      )}
                     </div>
                   </Table.Td>
                 </Table.Tr>
@@ -299,7 +330,7 @@ export default function GeneralSettings() {
             </Button>
           </Table.Caption>
         </Table>
-      )}
+      </Collapse>
     </>
   );
 }