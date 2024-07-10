1e018cdb8dfb60b7df488eb01ad5d56620a5a9f4
Auro <35087590+MrAuro@users.noreply.github.com>
2024-07-10 17:21:57 -0400

    ui: Improve UI to be easier to use

--- src/components/CameraMenu.tsx
+++ src/components/CameraMenu.tsx
@@ -1,45 +1,49 @@
 import {
   Button,
+  Center,
+  Code,
+  Divider,
+  Flex,
   Image,
   Paper,
-  Progress,
   ScrollArea,
-  Select,
   Text,
   useMantineTheme,
 } from "@mantine/core";
-import { useCallback, useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 import Webcam from "react-webcam";
 
-import { GoogleGenerativeAI } from "@google/generative-ai";
-import { useRecoilValue } from "recoil";
 import { SETTINGS_STATE } from "@/Root";
-import { Card, CardRank, CardRank_NOEMPTY, CardSuit_NOEMPTY } from "@/types/Card";
+import { Card, CardRank_NOEMPTY, CardSuit_NOEMPTY } from "@/types/Card";
+import { GoogleGenerativeAI } from "@google/generative-ai";
+import { useElementSize } from "@mantine/hooks";
+import { modals } from "@mantine/modals";
+import { IconCamera, IconPlus } from "@tabler/icons-react";
+import { useRecoilValue } from "recoil";
 import PlayingCard from "./PlayingCard";
-
-// [{"rank": "3", "suit": "d"}] is 28 characters long
-// If there are more cards, the length will be longer, but 28 is the minimum
-const ESTIMATED_RESULT_LENGTH = 28;
 
 export default function CameraMenu() {
   const theme = useMantineTheme();
   const [image, setImage] = useState<string | null>(null);
+  const [stats, setStats] = useState<string>("No image captured");
 
   const settings = useRecoilValue(SETTINGS_STATE);
 
-  const [aiResult, setAiResult] = useState<string>("");
+  const [rawResponse, setRawResponse] = useState<string>("");
   const [cards, setCards] = useState<Card[]>([]);
+  const [loading, setLoading] = useState<boolean>(false);
 
   const genAI = new GoogleGenerativeAI(settings.geminiApiKey);
 
   const model = genAI.getGenerativeModel({
     model: "gemini-1.5-flash",
     systemInstruction:
-      'You are a playing card identifying bot. You will be given an image and you must determine which suit and rank the card is. You must respond in JSON format. You must respond with a rank and suit field. You must give the suit in the abbreviated form (s for Spades, h for Hearts, c for Clubs, d for Diamonds). You must give the rank in the abbreviated form (A for Ace, 2-9 for cards 2-9, T for 10, J for Jacks, Q for Queens, K for Kings). [{"rank": _, "suit": _}]. Return an array of card or cards.',
+      'Determine which suit and rank the card is. You must respond in JSON format with a rank and suit field. You must give the suit in the abbreviated form (s for Spades, h for Hearts, c for Clubs, d for Diamonds). You must give the rank in the abbreviated form (A for Ace, 2-9 for cards 2-9, T for 10, J for Jacks, Q for Queens, K for Kings). [{"rank": _, "suit": _}]. Return an array of card or cards.',
   });
 
   const webcamRef = useRef<Webcam>(null);
+  const webcamElementSize = useElementSize();
 
   useEffect(() => {
     (async () => {
@@ -59,9 +63,9 @@ export default function CameraMenu() {
       // and just send the base64 part
       const imageBase64 = image.split(",")[1];
 
-      setAiResult("");
-
-      const result = await model.generateContentStream([
+      setLoading(true);
+      const t0 = performance.now();
+      const result = await model.generateContent([
         {
           inlineData: {
             data: imageBase64,
@@ -70,50 +74,85 @@ export default function CameraMenu() {
         },
       ]);
 
-      for await (const chunk of result.stream) {
-        const chunkText = chunk.text();
-        setAiResult((prev) => prev + chunkText);
-      }
-
       const response = await result.response;
       const text = await response.text();
       console.log(`AI Response: ${text}`);
       const cardData: { rank: CardRank_NOEMPTY; suit: CardSuit_NOEMPTY }[] = JSON.parse(text);
+      setRawResponse(text);
+      const t1 = performance.now();
       setCards(cardData.map((card) => `${card.rank}${card.suit}` as Card));
+      setLoading(false);
+
+      setStats(`${(t1 - t0).toFixed(2)}ms • ${response.usageMetadata?.totalTokenCount} tokens`);
     })();
   }, [image]);
 
   return (
     <ScrollArea m="xs" scrollbars="y" type="never">
-      <Paper
-        withBorder
-        p="xs"
-        pt={2}
-        style={{
-          backgroundColor: theme.colors.dark[7],
-        }}
-      >
+      <div ref={webcamElementSize.ref}>
         <Webcam
           width="100%"
           ref={webcamRef}
           audio={false}
           screenshotFormat="image/png"
           videoConstraints={{ deviceId: settings.cameraDeviceId }}
-        />
-        <Button
-          onClick={async () => {
-            if (!webcamRef.current) return;
-            setImage(webcamRef.current.getScreenshot());
+          style={{
+            borderRadius: theme.radius.md,
           }}
-        >
-          Detect
-        </Button>
-        <Progress size="sm" value={(aiResult.length / ESTIMATED_RESULT_LENGTH) * 100} />
-        <Image src={image || ""} />
+        />
+      </div>
+      <Button
+        fullWidth
+        leftSection={<IconCamera />}
+        loading={loading}
+        onClick={async () => {
+          if (!webcamRef.current) return;
+          setImage(webcamRef.current.getScreenshot());
+        }}
+      >
+        Capture
+      </Button>
+      <Divider my="sm" />
+
+      <Flex justify="center" gap="md" mt="sm">
         {cards.map((card, index) => (
-          <PlayingCard key={index} onClick={() => {}} disabled card={card} />
+          <Paper key={index} p="xs" radius="lg" withBorder>
+            <Center>
+              <PlayingCard key={index} onClick={() => {}} disabled card={card} />
+            </Center>
+            <Flex justify="center" gap="xs" mx="xs">
+              <Button leftSection={<IconPlus />} color="green" variant="light" mt="xs">
+                Add
+              </Button>
+            </Flex>
+          </Paper>
         ))}
-      </Paper>
+        {cards.length === 0 && <Text c="dimmed">No cards detected</Text>}
+      </Flex>
+
+      <Divider my="sm" />
+      <Text
+        c="dimmed"
+        ta="center"
+        size="sm"
+        style={{
+          cursor: "pointer",
+        }}
+        title="View image capture"
+        onClick={() => {
+          modals.open({
+            title: "Image Capture",
+            children: (
+              <>
+                <Code>{rawResponse}</Code>
+                <Image src={image} radius="lg" />
+              </>
+            ),
+          });
+        }}
+      >
+        {stats}
+      </Text>
     </ScrollArea>
   );
 }
