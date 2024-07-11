a79cb98e193c21cb3a632bf0cdb040d35233536d
Auro <35087590+MrAuro@users.noreply.github.com>
2024-07-11 16:07:33 -0400

    ui: Add logo to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,8 @@
-import { Container, Group, Tabs, Text, rem } from "@mantine/core";
+import { ActionIcon, Container, Flex, Group, Image, Tabs, Text, rem } from "@mantine/core";
 import { PAGES, Page } from "../types/State";
 import { IconCards, IconClubs, IconSettings, IconUsers } from "@tabler/icons-react";
+import { useState } from "react";
+import shuffle from "lodash/shuffle";
 
 export default function Header({
   active,
@@ -9,12 +11,26 @@ export default function Header({
   active: string;
   setActive: (page: Page) => void;
 }) {
+  const [title, setTitle] = useState("Card Games Manager");
+
   return (
     <Container size="md" mt="sm">
       <Group gap={5} justify={false ? "center" : "space-between"}>
-        <Text size={rem(26)} fw="bold">
-          Card Games Manager
-        </Text>
+        <Flex align="center" gap={5}>
+          <ActionIcon
+            radius={0}
+            variant="transparent"
+            size="lg"
+            onClick={() => {
+              setTitle(shuffle(title.split(" ")).join(" "));
+            }}
+          >
+            <Image src="app-icon.png" />
+          </ActionIcon>
+          <Text size={rem(26)} fw="bold">
+            {title}
+          </Text>
+        </Flex>
         <Tabs variant="pills" radius="xl">
           <Tabs.List>
             {PAGES.map((page) => {
