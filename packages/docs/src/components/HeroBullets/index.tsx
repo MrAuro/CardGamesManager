38226b3ca40c12fb350f15a1cf2d678af8c249eb
Auro <35087590+MrAuro@users.noreply.github.com>
2024-07-27 17:24:23 -0400

    ui(docs): Animate the hero svg

--- packages/docs/src/components/HeroBullets/index.tsx
+++ packages/docs/src/components/HeroBullets/index.tsx
@@ -1,57 +1,54 @@
-import { Image, Container, Title, Button, Group, Text, List, ThemeIcon, rem } from "@mantine/core";
-import { IconCheck } from "@tabler/icons-react";
-import classes from "./styles.module.css";
+import { Container, Grid, Title, alpha, rem, useMantineTheme } from "@mantine/core";
 
 const Svg = require("@site/static/img/hero-main.svg").default;
 
 export function HeroBullets() {
+  const theme = useMantineTheme();
+
   return (
-    <Container size="lg">
-      <div className={classes.inner}>
-        <div className={classes.content}>
-          <Title c="white" fw={800} size={rem(50)}>
-            Deal With <span className={classes.highlight}>Confidence</span> <br /> Not Chaos
-          </Title>
-          <Text c="dimmed" mt="md">
-            Build fully functional accessible web applications faster than ever – Mantine includes
-            more than 120 customizable components and hooks to cover you in any situation
-          </Text>
-
-          <List
-            mt={30}
-            spacing="sm"
-            size="sm"
-            icon={
-              <ThemeIcon size={20} radius="xl">
-                <IconCheck style={{ width: rem(12), height: rem(12) }} stroke={1.5} />
-              </ThemeIcon>
-            }
+    <Container size="xl" mt="xl" mx="xl">
+      <Grid grow gutter="xl">
+        <Grid.Col
+          span={7}
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+          }}
+        >
+          <Title
+            c="white"
+            size={rem(50)}
+            style={{
+              fontFamily: "Comfortaa, Nunito, sans-serif",
+            }}
           >
-            <List.Item>
-              <b>TypeScript based</b> – build type safe applications, all components and hooks
-              export types
-            </List.Item>
-            <List.Item>
-              <b>Free and open source</b> – all packages have MIT license, you can use Mantine in
-              any project
-            </List.Item>
-            <List.Item>
-              <b>No annoying focus ring</b> – focus ring will appear only when user navigates with
-              keyboard
-            </List.Item>
-          </List>
-
-          <Group mt={30}>
-            <Button radius="xl" size="md" className={classes.control}>
-              Get started
-            </Button>
-            <Button variant="default" radius="xl" size="md" className={classes.control}>
-              Source code
-            </Button>
-          </Group>
-        </div>
-        <Image src={"/img/hero-main.svg"} visibleFrom="md" width="376px" height="376px" />
-      </div>
+            Deal With{" "}
+            <span
+              style={{
+                position: "relative",
+                backgroundColor: alpha("var(--mantine-color-blue-7)", 0.6),
+                borderRadius: rem(16),
+                padding: `${rem(4)} ${rem(4)}`,
+              }}
+            >
+              {/* Makes the fi in Confidence not so close together */}
+              Con<span style={{ letterSpacing: "0.1px" }}>f</span>idence
+            </span>{" "}
+            <br /> Not Chaos
+          </Title>
+        </Grid.Col>
+        <Grid.Col
+          span={5}
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+          }}
+        >
+          <object type="image/svg+xml" data="/img/hero-main.svg"></object>
+        </Grid.Col>
+      </Grid>
     </Container>
   );
 }
