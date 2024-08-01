0de244392c7e50b3eabe0ec43beca39cbdd57de9
Auro <35087590+MrAuro@users.noreply.github.com>
2024-08-01 14:36:51 -0400

    fix(desktop): improve data loading and migration

--- packages/desktop/src/types/Poker.ts
+++ packages/desktop/src/types/Poker.ts
@@ -2,6 +2,22 @@ import { Card } from "./Card";
 
 export type GameState = "PREROUND" | "BLINDS" | "PREFLOP" | "FLOP" | "TURN" | "RIVER" | "SHOWDOWN";
 
+export const DEFAULT_POKER_GAME: PokerGame = {
+  gameState: "PREROUND",
+  currentTurn: "",
+  communityCards: [],
+  currentBet: 0,
+  currentDealer: "",
+  currentSmallBlind: "",
+  currentBigBlind: "",
+  pots: [],
+  capturingCommunityCards: false,
+  runningThroughShowdown: false,
+  runningItTwice: false,
+  currentBets: {},
+  initialBalances: {},
+};
+
 export type PokerGame = {
   gameState: GameState;
   currentTurn: string;
@@ -15,12 +31,45 @@ export type PokerGame = {
   runningThroughShowdown: boolean; // Everyone (or everyone minus one) has gone all in and we're just showing the cards
   runningItTwice: boolean;
   currentBets: {
-    [playerId: string]: {
-      amount: number;
-      dontAddToPot: boolean;
-    };
+    [playerId: string]: PokerCurrentBet;
   };
   initialBalances: Record<string, number>; // Player ID to initial balance
+};
+
+export const DEFAULT_POKER_POT: PokerPot = {
+  eligiblePlayers: [],
+  amount: 0,
+  closed: false,
+  winnerOverrides: [],
+};
+
+export type PokerPot = {
+  eligiblePlayers: string[];
+  amount: number;
+  /**
+   * @deprecated
+   */
+  maximum?: number;
+  closed: boolean;
+  winnerOverrides: string[];
+};
+
+export const DEFAULT_POKER_CURRENT_BET: PokerCurrentBet = {
+  amount: 0,
+  dontAddToPot: false,
+};
+
+export type PokerCurrentBet = {
+  amount: number;
+  dontAddToPot: boolean;
+};
+export const DEFAULT_POKER_RESULT: PlayerResult = {
+  handRank: null,
+  hand: null,
+  ties: 0,
+  tiesPercentage: "0%",
+  win: 0,
+  winPercentage: "0%",
 };
 
 export type PlayerResult = {
@@ -32,9 +81,24 @@ export type PlayerResult = {
   winPercentage: string;
 };
 
+export const DEFAULT_STORED_PLAYER_RESULT: StoredPlayerResult = {
+  id: "",
+  result: DEFAULT_POKER_RESULT,
+};
+
 export type StoredPlayerResult = {
   id: string;
   result: PlayerResult;
+};
+
+export const DEFAULT_POKER_PLAYER: PokerPlayer = {
+  id: "",
+  cards: [],
+  displayName: "",
+  folded: false,
+  currentBet: 0,
+  allIn: false,
+  beenOn: false,
 };
 
 export type PokerPlayer = {
@@ -47,20 +111,16 @@ export type PokerPlayer = {
   beenOn: boolean; // Whether or not the player has acted in the current round
 };
 
+export const DEFAULT_POKER_SETTINGS: PokerSettings = {
+  forcedBetOption: "BLINDS",
+  smallBlind: 5,
+  bigBlind: 10,
+  ante: 0,
+};
+
 export type PokerSettings = {
   forcedBetOption: "BLINDS" | "ANTE" | "BLINDS+ANTE";
   smallBlind: number;
   bigBlind: number;
   ante: number;
-};
-
-export type PokerPot = {
-  eligiblePlayers: string[];
-  amount: number;
-  /**
-   * @deprecated
-   */
-  maximum?: number;
-  closed: boolean;
-  winnerOverrides: string[];
 };
